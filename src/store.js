57bac3f9bb26d0285b69e7488443916fdcac3349
alex02r <alex.giochi02@gmail.com>
2024-01-12 12:21:50 +0100

    [UPGRADE] Add parameters on global state file

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-    
+    APIKey: 'b07c7d12dec7a7e46d131121599a8195',
+    urlFilms: 'https://api.themoviedb.org/3/search/movie?api_key=',
+    urlTv: 'https://api.themoviedb.org/3/search/tv?api_key=',
 })
