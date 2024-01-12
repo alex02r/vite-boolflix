3651820cf5d59f5e046d90e1d86831930445ffa6
alex02r <alex.giochi02@gmail.com>
2024-01-13 00:24:43 +0100

    [UPDATE] Add logic of components

--- src/components/AppVote.vue
+++ src/components/AppVote.vue
@@ -1,14 +1,45 @@
 <script>
 import { store } from "../store";
 export default {
+    props:{
+        vote_value: Number
+    },
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        
+        //funcion for get vote in stars
+        getStarVote(num){
+            let stars = []
+            let n = Math.floor(num) / 2;
+            n = Math.round(n);
+            for (let i = 0; i < n; i++) {
+                stars.push(1)
+            }
+            return stars
+        },
+        getStarEmpty(num){
+            let empty = [];
+            let n = Math.floor(num) / 2;
+            n = Math.round(n)
+            let length = 5 - n;
+            for (let i = 0; i < length; i++) {
+                empty.push(0)
+            }
+            return empty
+        }
+    },
 }
 </script>
 <template lang="">
     <div class="star-container">
-        <div class="star">
+        <div class="star" v-for="(star, index) in getStarVote(vote_value)" :key="index">
             &#9733;
         </div>
-        <div class="star">
+        <div class="star" v-for="(empty, index) in getStarEmpty(vote_value)" :key="index">
             &#9734;
         </div>
     </div>
