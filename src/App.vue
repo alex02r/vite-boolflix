90d52c9a63de2b1019884dbc5092e7023a9f78f5
alex02r <alex.giochi02@gmail.com>
2024-01-12 14:06:04 +0100

    [UPGRADE] copy array films result in arry on store file

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,11 @@ export default {
   methods: {
     //function for request list of film at API
     search_title(){
+      store.arrayFilms = []
       axios.get(`${store.urlFilms}${store.APIKey}&query=${store.searchValue}&language=it-IT`).then( response =>{
-        console.log(response.data.results);
+        /* console.log(response.data.results); */
+        store.arrayFilms = [...response.data.results];
+        console.log(store.arrayFilms);
       })
     }
   },
