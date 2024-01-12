57acde4b5d7d30d2c8215c1fa95cf59da978e842
alex02r <alex.giochi02@gmail.com>
2024-01-13 00:24:07 +0100

    [UPDATE] Add structure of list stars

--- src/components/AppVote.vue
+++ src/components/AppVote.vue
@@ -1,9 +1,24 @@
 <script>
+import { store } from "../store";
 export default {
 }
 </script>
 <template lang="">
+    <div class="star-container">
+        <div class="star">
+            &#9733;
+        </div>
+        <div class="star">
+            &#9734;
+        </div>
+    </div>
 </template>
 <style lang="scss" scoped>
 @use '../styles/generals.scss';
+    .star-container{
+        display: flex;
+        .star{
+            font-size: 20px;
+        }
+    }
 </style>
