906a9723d4c36269cd893ae18db9e7aca34b709c
alex02r <alex.giochi02@gmail.com>
2024-01-12 13:04:57 +0100

    [UPGRADE] Add structure for search bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,14 +5,25 @@ export default {
 </script>
 <template lang="">
     <header>
-        
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    logo
+                </div>
+                <div class="col-6">
+                    <div class="input-group m-3">
+                        <input type="text" class="form-control" placeholder="Film, SerieTv">
+                        <button class="btn btn-secondary">Cerca</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 <style lang="scss" scoped>
     @use '../styles/partials/variables' as *;
     @use '../styles/generals.scss';
     header{
-        height: 100px;
         background-color: $black;
     }
 </style>
