f2ae576d573a50d4dc69919d66397738efb9d6d3
alex02r <alex.giochi02@gmail.com>
2024-01-13 18:20:33 +0100

    [UPGRADE] Add scss for cards of films

--- src/components/AppFilms.vue
+++ src/components/AppFilms.vue
@@ -30,23 +30,26 @@ export default {
             <h1>Film</h1>
         </div>
         <div class="col-4 col-md-3 col-lg-2" v-for="(film, index) in store.arrayFilms" :key="index">
-            <div class="thumb">
-                <img :src="`http://image.tmdb.org/t/p/w500${film.poster_path}`" :alt="film.title">
+            <div class="info">
+                <div class="thumb">
+                    <img :src="`http://image.tmdb.org/t/p/w342${film.poster_path}`" :alt="film.title">
+                </div>
+                <ul class="list-unstyled">
+                    <li>{{ film.title }}</li>
+                    <li>{{ film.original_title }}</li>
+                    <li>
+                        Lingua originale: <img class="flag" :src="getFlag(film.original_language)" :alt="film.original_language">
+                    </li>
+                    <li>
+                        <AppVote :vote_value="film.vote_average"/>
+                    </li>
+                </ul>
             </div>
-            <ul class="list-unstyled">
-                <li>{{ film.title }}</li>
-                <li>{{ film.original_title }}</li>
-                <li>
-                    Lingua originale: <img class="flag" :src="getFlag(film.original_language)" :alt="film.original_language">
-                </li>
-                <li>
-                    <AppVote :vote_value="film.vote_average"/>
-                </li>
-            </ul>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    @use '../styles/partials/variables' as *;
     @use '../styles/generals.scss';
+    @use '../styles/movies.scss';
+    
 </style>
