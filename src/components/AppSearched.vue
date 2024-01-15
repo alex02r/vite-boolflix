99a5fcf5fa7591927d758856838ce40826d328c3
alex02r <alex.giochi02@gmail.com>
2024-01-15 15:59:09 +0100

    [UPGRADE] Create new components For searched content

---
+++ src/components/AppSearched.vue
@@ -0,0 +1,24 @@
+<script>
+import AppVote from "./AppVote.vue";
+import { store } from "../store";
+export default {
+    components:{
+        AppVote
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+    },
+}
+</script>
+<template lang="">
+
+    
+</template>
+<style lang="scss" scoped>
+    @use '../styles/generals.scss';
+    @use '../styles/movies.scss';
+</style>
