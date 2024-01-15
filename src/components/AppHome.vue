4758a91e85e4ac0ff623db9ca9fa52fd4dc48c35
alex02r <alex.giochi02@gmail.com>
2024-01-15 11:41:29 +0100

    [UPGRADE] Add methods for request at API

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,13 +1,35 @@
 <script>
+import axios from "axios";
+import { store } from "../store";
 export default {
-    
+    data() {
+        return {
+            store
+        }
+    },
+    created() {
+        this.getHome();
+    },
+    methods: {
+        getHome(){
+            store.popularFilms = [];
+            store.popularSeries = [];
+            axios.get(`${store.urlPopularFilms}${store.APIKey}&language=it-IT`).then(result =>{
+                store.popularFilms = result.data.results;
+            })
+            axios.get(`${store.urlPopularTV}${store.APIKey}&language=it-IT`).then(res =>{
+                store.popularSeries = res.data.results;
+            })
+        }
+    },
 }
 </script>
 <template lang="">
-    <div>
+    <div class="home">
         
     </div>
 </template>
 <style lang="scss" scoped>
-    
+    @use '../styles/generals.scss';
+    @use '../styles/movies.scss'
 </style>
--- src/store.js
+++ src/store.js
@@ -4,8 +4,12 @@ export const store = reactive({
     APIKey: 'b07c7d12dec7a7e46d131121599a8195',
     urlFilms: 'https://api.themoviedb.org/3/search/movie?api_key=',
     urlTv: 'https://api.themoviedb.org/3/search/tv?api_key=',
+    urlPopularFilms: 'https://api.themoviedb.org/3/movie/popular?api_key=',
+    urlPopularTV: 'https://api.themoviedb.org/3/tv/popular',
     searchValue: '',
     arrayFilms: [],
     arrayTv: [],
-    searched: false
+    searched: false,
+    popularFilms: [],
+    popularSeries: []
 })
