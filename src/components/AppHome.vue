4e4a0bdd298b0028573ca53473bee50f8feca6a0
alex02r <alex.giochi02@gmail.com>
2024-01-15 14:05:56 +0100

    [UPGRADE] Add sliders for all list of series

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -49,21 +49,30 @@ export default {
 }
 </script>
 <template lang="">
-    <h1>Film più visti</h1>
-    <div class="home-content">
-        <div class="slider">
-            <img :src="`https://image.tmdb.org/t/p/w1280${store.popularFilms[currentFilm].backdrop_path}`" :alt="store.popularFilms[currentFilm].title">
-            <h1>{{ store.popularFilms[currentFilm].title }}</h1>
+    <div class="container-fluid">
+        <h1>Film più visti</h1>
+        <div class="home-content">
+            <div class="slider">
+                <img :src="`https://image.tmdb.org/t/p/w1280${store.popularFilms[currentFilm].backdrop_path}`" :alt="store.popularFilms[currentFilm].title">
+                <h1>{{ store.popularFilms[currentFilm].title }}</h1>
+            </div>
+        </div>
+        <h5 class="text-white">Tutti i film più visti</h5>
+        <div class="lists">
+            <img v-for="(film, index) in store.popularFilms" :key="index" :src="`https://image.tmdb.org/t/p/w500${film.poster_path}`" :alt="film.title">
+        </div>
+        <h1>Serie Tv più viste</h1>
+        <div class="home-content">
+            <div class="slider">
+                <img :src="`https://image.tmdb.org/t/p/w1280${store.popularSeries[currentSeries].backdrop_path}`" :alt="store.popularSeries[currentSeries].name">
+                <h1>{{ store.popularSeries[currentSeries].name }}</h1>
+            </div>
+        </div>
+        <h5 class="text-white">Tutte le serieTV più visti</h5>
+        <div class="lists">
+            <img v-for="(series, index) in store.popularSeries" :key="index" :src="`https://image.tmdb.org/t/p/w500${series.poster_path}`" :alt="series.name">
         </div>
     </div>
-    <h1>Serie Tv più viste</h1>
-    <div class="home-content">
-        <div class="slider">
-            <img :src="`https://image.tmdb.org/t/p/w1280${store.popularSeries[currentSeries].backdrop_path}`" :alt="store.popularSeries[currentSeries].name">
-            <h1>{{ store.popularSeries[currentSeries].name }}</h1>
-        </div>
-    </div>
-
 </template>
 <style lang="scss" scoped>
     @use '../styles/generals.scss';
@@ -82,6 +91,14 @@ export default {
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
+        }
+    }
+    .lists{
+        margin: 10px;
+        display: flex;
+        overflow-x: auto;
+        img{
+            width: 200px;
         }
     }
 </style>
