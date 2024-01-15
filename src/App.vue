fcd8694a65d04202fc950787e5d9b2f4e063115a
alex02r <alex.giochi02@gmail.com>
2024-01-15 14:34:42 +0100

    [FIX] Change methods for assigment of array result

--- src/App.vue
+++ src/App.vue
@@ -30,14 +30,14 @@ export default {
       store.arrayFilms = []
       axios.get(`${store.urlFilms}${store.APIKey}&query=${store.searchValue}&language=it-IT`).then( response =>{
         /* console.log(response.data.results); */
-        store.arrayFilms = [...response.data.results];
+        store.arrayFilms = response.data.results;
       })
 
       //serie tv list
       store.arrayTv = []
       axios.get(`${store.urlTv}${store.APIKey}&language=it-IT&query=${store.searchValue}`).then( data =>{
         /* console.log(response.data.results); */
-        store.arrayTv = [...data.data.results];
+        store.arrayTv = data.data.results;
       })
     }
   },
