77e9373f12dbe85456ce79f9002c63f92918aaf5
alex02r <alex.giochi02@gmail.com>
2024-01-15 11:15:01 +0100

    [UPGRADE] Add style at header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,15 +10,23 @@ export default {
 </script>
 <template lang="">
     <header>
-        <div class="container">
+        <div class="container-fluid">
             <div class="row justify-content-between">
-                <div class="col-2">
-                    <div class="logo">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="logo netflix">
+                <div class="col-8">
+                    <div class="d-flex h-100 gap-3">
+                        <div class="logo">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="logo netflix">
+                        </div>
+                        <ul class="list-header">
+                            <li>Home</li>
+                            <li>Film</li>
+                            <li>Serie TV</li>
+                            <li>La mia lista</li>
+                        </ul>
                     </div>
                 </div>
-                <div class="col-6">
-                    <div class="input-group m-3">
+                <div class="col-2">
+                    <div class="input-group input-group-sm d-flex align-items-center h-100">
                         <!-- Searchbar with emit on App.vue -->
                         <input type="text" class="form-control" placeholder="Film, SerieTv" v-model="store.searchValue" @keyup.enter="$emit('SearchTitle')">
                         <button class="btn btn-secondary" @click="$emit('SearchTitle')">Cerca</button>
@@ -32,12 +40,26 @@ export default {
     @use '../styles/partials/variables' as *;
     @use '../styles/generals.scss';
     header{
-        padding: 20px 0;
+        padding: 5px 0;
         background-color: $black;
         .logo{
             display: flex;
             align-items: center;
             height: 100%;
+            width: 80px;
         }
+        .list-header{
+            display: flex;
+            align-items: center;
+            gap: 10px;
+            height: 100%;
+            list-style: none;
+            color: $white;
+            cursor: pointer;
+            li:hover, li:active{
+                color: rgb(220, 53, 69);
+            }
+        }
+        
     }
 </style>
